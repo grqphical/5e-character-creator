e8d1e5d65da8f137f7e56eb28ffcf633fa1c34f4
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2026-02-24 14:01:26 -0800

    finished basic character sheet JSON model

--- src/models.ts
+++ src/models.ts
@@ -14,6 +14,55 @@ enum Class {
     Wizard = "Wizard"
 }
 
+enum Ability {
+    STR = "STR",
+    DEX = "DEX",
+    CON = "CON",
+    INT = "INT",
+    WIS = "WIS",
+    CHA = "CHA"
+}
+
+enum Dice {
+    D4 = "D4",
+    D6 = "D6",
+    D8 = "D8",
+    D10 = "D10",
+    D12 = "D12",
+    D20 = "D20",
+    D100 = "D100"
+}
+
+interface Attack {
+    name: string
+    attack_bonus: number
+    damage: string 
+}
+
+interface Spell {
+    name: string
+    level: number
+    casting_time: string
+    range_area: string
+    verbal: boolean
+    somatic: boolean
+    material: boolean
+    duration: string
+    school: string
+    attack_save: string
+    description: string
+}
+
+interface Spellcasting {
+    class: Class
+    spellcasting_ability: Ability
+    save_dc: number
+    spell_attack_bonus: number
+    spell_slots: Array<number>
+    used_spell_slots: Array<number>
+    spells: Spell
+}
+
 export interface Character {
     name: string
     class: Class
@@ -22,13 +71,83 @@ export interface Character {
     background: string
     level: number
     xp: number | null
+    alignment: string
+
+    age: number
+    height: string
+    weight: string
+    eyes: string
+    skin: string
+    hair: string
+
     proficiency_bonus: number
-    armor_class: number
     max_hp: number
     current_hp: number
     temporary_hp: number
+    hit_dice: Dice
     max_hit_dice: number
-    current_hit_dict: number
+    current_hit_dice: number
     death_saving_throws: Array<boolean>
     inspiration: boolean
+
+    initiative: number
+    speed: number
+    armor_class: number
+    
+
+    strength: number
+    dexterity: number
+    constitution: number
+    intelligence: number
+    wisdom: number
+    charisma: number
+
+    passive_perception: number
+
+    strength_saving_throw: number
+    dexterity_saving_throw: number
+    constitution_saving_throw: number
+    intelligence_saving_throw: number
+    wisdom_saving_throw: number
+    charisma_saving_throw: number
+
+    acrobatics: number
+    animal_handling: number
+    arcana: number
+    athletics: number
+    deception: number
+    history: number
+    insight: number
+    intimidation: number
+    investigation: number
+    medicine: number
+    nature: number
+    perception: number
+    performance: number
+    persuasion: number
+    religion: number
+    sleight_of_hand: number
+    stealth: number
+    survival: number
+
+    copper: number
+    electrum: number
+    platinum: number
+    gold: number
+    silver: number
+
+    personality_traits: string
+    ideals: string
+    bonds: string
+    flaws: string
+
+    attacks: Map<string, Attack>
+    languages: Array<string>
+    proficiencies: Array<string>
+    features_traits: Array<string>
+    allies_organizations: Array<string>
+    backstory: string
+    treasure: Array<string>
+
+    spellcasting: Spellcasting | null
 }
