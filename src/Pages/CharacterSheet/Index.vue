2288063b9304f1484412cebdd9aaf09707e97fa7
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-10 14:51:42 -0700

    broke out stats into seperate component

--- src/Pages/CharacterSheet/Index.vue
+++ src/Pages/CharacterSheet/Index.vue
@@ -5,6 +5,8 @@ import races from "../../Data/races.json"
 import { useHead } from '@unhead/vue'
 import { ClassName } from '../../models';
 import Header from "./Header.vue"
+import Stats from './Stats.vue';
+import { abilityToModifierLoookup } from './utils';
 
 const characterStore = useCharacterStore();
 
@@ -19,39 +21,6 @@ if (character !== undefined) {
         title: `${character?.name} - CharacterForge` || '404 - Character Not Found'
     })
 }
-
-const abilityToModifierLoookup = new Map<number, number>([
-    [1, -5],
-    [2, -4],
-    [3, -4],
-    [4, -3],
-    [5, -3],
-    [6, -2],
-    [7, -2],
-    [8, -1],
-    [9, -1],
-    [10, 0],
-    [11, 0],
-    [12, 1],
-    [13, 1],
-    [14, 2],
-    [15, 2],
-    [16, 3],
-    [17, 3],
-    [18, 4],
-    [19, 4],
-    [20, 5],
-    [21, 5],
-    [22, 6],
-    [23, 6],
-    [24, 7],
-    [25, 7],
-    [26, 8],
-    [27, 8],
-    [28, 9],
-    [29, 9],
-    [25, 10],
-])
 
 const proficiencyBonusLookup: Record<number, number> = {
     1: 2,
@@ -76,20 +45,6 @@ const proficiencyBonusLookup: Record<number, number> = {
     20: 6,
 }
 
-const getModifierFromAbility = (abilityScore: number): string => {
-    const modifier = abilityToModifierLoookup.get(abilityScore)
-    if (modifier === undefined) {
-        console.error("could not lookup modifier for", abilityScore)
-    }
-    const sign = Math.sign(modifier!) === -1 ? "-" : "+"
-    return `${sign}${Math.abs(modifier!)}`
-}
-
-const getAbility = (name: string): number | undefined => {
-    const key = name as keyof typeof character.stats;
-    return character.stats[key] + character.chosen_stats_bonuses[key];
-}
-
 const toggleInspiration = () => {
     const newInspiration = !character.inspiration
     character.inspiration = newInspiration;
@@ -106,51 +61,7 @@ const toggleInspiration = () => {
 
     <Header :character="character" />
     <div class="flex flex-row h-full mt-2 gap-3">
-        <div class="flex flex-col gap-3">
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("str")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("str")! }}</p>
-                <p class="font-bold">STR</p>
-            </div>
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("dex")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("dex")! }}</p>
-                <p class="font-bold">DEX</p>
-            </div>
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("con")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("con")! }}</p>
-                <p class="font-bold">CON</p>
-            </div>
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("int")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("con") }}</p>
-                <p class="font-bold">INT</p>
-            </div>
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("wis")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("wis") }}</p>
-                <p class="font-bold">WIS</p>
-            </div>
-            <div class="w-25 bg-white rounded-md shadow-xl p-2 flex flex-col justify-center items-center">
-                <p class="text-3xl border-2 rounded aspect-square text-center p-0.5 mb-1">{{
-                    getModifierFromAbility(getAbility("cha")!) }}
-                </p>
-                <p class="border aspect-square text-center rounded">{{ getAbility("cha") }}</p>
-                <p class="font-bold">CHA</p>
-            </div>
-
-        </div>
+        <Stats :character="character" />
         <div class="flex flex-col gap-2">
             <div class="w-60 bg-white rounded-md shadow-xl p-1 flex flex-row gap-2 items-center">
                 <label class="relative flex items-center cursor-pointer text-xl font-bold">
